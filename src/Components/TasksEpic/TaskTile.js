a95a2f23e444a3318da4300eccb31659b2091606
Benjamin Desole <69907169+BenGex31@users.noreply.github.com>
2021-06-28 15:57:36 +0200

    Update TaskTile / TasksList components

--- src/Components/TasksEpic/TaskTile.js
+++ src/Components/TasksEpic/TaskTile.js
@@ -1,11 +1,37 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 const TaskTile = ({ title }) => {
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.container}>
+      <View style={styles.subContainer}>
+        <Image
+          style={styles.icon}
+          source={require("../../assets/icon_check.png")}
+        />
+        <Text>{title}</Text>
+      </View>
+
+      <Image
+        style={styles.icon}
+        source={require("../../assets/icon_bin.png")}
+      />
     </View>
   );
 };
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  },
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  subContainer: {
+    flexDirection: "row",
+    alignItems: "center"
+  }
+});
 export default TaskTile;
--- src/Components/TasksEpic/TasksList.js
+++ src/Components/TasksEpic/TasksList.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { FlatList, Text } from "react-native";
+import TaskTile from "./TaskTile";
 
 const TasksList = ({ tasks }) => {
-  const _renderItem = ({ item }) => <Text>{item.title}</Text>;
+  const _renderItem = ({ item }) => <TaskTile title={item.title}></TaskTile>;
   return (
     <FlatList
       data={tasks}
