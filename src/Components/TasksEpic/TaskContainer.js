1f630776bf1fdfaf766dedfc34cad2d1411f818d
Benjamin Desole <69907169+BenGex31@users.noreply.github.com>
2021-06-28 14:22:56 +0200

    Update TaskContainer component

--- src/Components/TasksEpic/TaskContainer.js
+++ src/Components/TasksEpic/TaskContainer.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 
 function TaskContainer(props) {
+  const [tasks, setTasks] = useState([
+    { title: "Nouvelle tâche", completed: false }
+  ]);
   return (
     <View>
-      <Text>Tasks container</Text>
+      <Text>{tasks[0].title}</Text>
     </View>
   );
 }
