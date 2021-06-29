9edebe5e282b488b21aab95bc32a93c734fcf423
Benjamin Desole <69907169+BenGex31@users.noreply.github.com>
2021-06-29 17:51:59 +0200

    Update

--- src/Components/_Shared/FloatingButton/index.js
+++ src/Components/_Shared/FloatingButton/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 
-const FloatingButton = ({}) => {
+const FloatingButton = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>+</Text>
