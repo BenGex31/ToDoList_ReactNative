77d65bd8ff622e000d5392caed7c683ecdbfb8fe
Benjamin Desole <69907169+BenGex31@users.noreply.github.com>
2021-06-29 14:54:06 +0200

    Update

--- src/Components/TasksEpic/TaskContainer.js
+++ src/Components/TasksEpic/TaskContainer.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import TaskForm from "./taskForm";
 import TasksList from "./TasksList";
 
