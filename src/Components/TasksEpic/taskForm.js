d3cd946db512cae87cc4065afafa7fadc1fafdef
Benjamin Desole <69907169+BenGex31@users.noreply.github.com>
2021-06-29 14:43:49 +0200

    Update taskForm

--- src/Components/TasksEpic/taskForm.js
+++ src/Components/TasksEpic/taskForm.js
@@ -1,13 +1,36 @@
-import React from "react";
-import { View, TextInput } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, StyleSheet, Button } from "react-native";
 
 const TaskForm = ({}) => {
-  const _onChangeText = (value) => {};
+  const [title, setTitle] = useState("");
+  const _onChangeText = (value) => {
+    setTitle(value);
+  };
+  const _onPressBtn = () => {};
   return (
-    <View>
-      <TextInput value={"Enter"} onChangeText={_onChangeText} />
+    <View style={styles.container}>
+      <View style={styles.containerInput}>
+        <TextInput value={title} onChangeText={_onChangeText} />
+      </View>
+      <Button onPress={_onPressBtn} title="Ajouter" color="#841584" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 15
+  },
+  containerInput: {
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 4,
+    paddingLeft: 7,
+    width: "70%"
+  }
+});
 
 export default TaskForm;
